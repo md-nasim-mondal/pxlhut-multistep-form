c65e15bc0f3dd91ed38b7b917e3bfa57851bfcba
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-04-09 01:32:25 +0600

    made multi step form page

---
+++ src/app/multi-step-form/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { PersonalInfoStep } from "@/components/form-steps/PersonalInfo";
+import { AddressDetailsStep } from "@/components/form-steps/AddressDetails";
+import { AccountSetupStep } from "@/components/form-steps/AccountSetup";
+import { SummaryStep } from "@/components/form-steps/Summary";
+import { useFormStore } from "@/lib/form-store";
+
+export default function MultiStepForm() {
+  const { currentStep } = useFormStore();
+
+  return (
+    <div className='max-w-md mx-auto p-4 space-y-6'>
+      {/* <FormProgress /> */}
+      {currentStep === 1 && <PersonalInfoStep />}
+      {currentStep === 2 && <AddressDetailsStep />}
+      {currentStep === 3 && <AccountSetupStep />}
+      {currentStep === 4 && <SummaryStep />}
+    </div>
+  );
+}
