7553fa82a1f99152d67985ca3ea6e2c96ae9ad55
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-04-10 22:42:52 +0600

    changed position of providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ReactQueryProviders } from "./providers";
+import { ReactQueryProviders } from "@/providers/QueryClientProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
